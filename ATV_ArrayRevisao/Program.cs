9242a3850900c36678e5377c7a8f9756e78154f9
Sofiaantenor <sofiaantenor1418@gmail.com>
2021-09-28 16:38:38 -0300

    commit inicial

---
+++ ATV_ArrayRevisao/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ATV_ArrayRevisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Cantores = new string[5];
+
+            Cantores[0] = "Ivete Sangalo";
+            Cantores[1] = "Ed Sheeran";
+            Cantores[2] = "Chico Buarque";
+            Cantores[3] = "Shawn Mendes";
+            Cantores[4] = "Marilia Mendonça";
+
+            Console.WriteLine(" ");
+            Console.WriteLine(Cantores[1]);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   EXIBIÇÃO DA LISTA LISTA DE CANTORES COM LAÇO FOREACH:");
+
+            foreach (string cant in Cantores)
+            {
+                Console.WriteLine(cant);
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("   EXIBIÇÃO DA LISTA LISTA DE CANTORES COM LAÇO FOR:");
+
+            for (int i = 0; i < Cantores.Length; i++)
+           {
+                Console . WriteLine (Cantores[i]);
+           }
+        
+            Console.WriteLine(" ");
+
+        }
+    }
+}
